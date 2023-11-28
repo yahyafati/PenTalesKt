a592e25c086665e9a19294a0cc6056ad46d1ed4c
Yahya <Haji>
2023-11-28 11:14:22 +0300

    added goodreadsAuthorId to Author model

--- src/main/kotlin/org/pentales/pentalesrest/models/Author.kt
+++ src/main/kotlin/org/pentales/pentalesrest/models/Author.kt
@@ -4,16 +4,21 @@ import jakarta.persistence.*
 import jakarta.validation.constraints.*
 
 @Entity
+@Table(
+    indexes = [Index(name = "author_goodreads_author_id_idx", columnList = "goodReadsAuthorId", unique = true)]
+)
 class Author(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     override var id: Long = 0L,
     @field:NotBlank
-    var name: String = ""
+    var name: String = "",
+    @Column(nullable = true)
+    var goodReadsAuthorId: Long = 0L,
 ) : IModel() {
 
     override fun toString(): String {
-        return "Author(id=$id, name='$name')"
+        return "Author(id=$id, name='$name', goodReadsAuthorId=$goodReadsAuthorId)"
     }
 }
 
