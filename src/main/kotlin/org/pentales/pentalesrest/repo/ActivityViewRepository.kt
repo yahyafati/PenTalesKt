8bd5bc222e0d2e2b281f6df21bb6ab81c2888425
Yahya Fati <yfati037@gmail.com>
2024-01-28 22:59:55 +0300

    fixed activity duplication by adding id

--- src/main/kotlin/org/pentales/pentalesrest/repo/ActivityViewRepository.kt
+++ src/main/kotlin/org/pentales/pentalesrest/repo/ActivityViewRepository.kt
@@ -3,8 +3,9 @@ package org.pentales.pentalesrest.repo
 import org.pentales.pentalesrest.models.enums.*
 import org.pentales.pentalesrest.models.view.*
 import org.pentales.pentalesrest.repo.base.*
+import java.util.*
 
-interface ActivityViewRepository : IReadOnlyRepository<ActivityView, Long> {
+interface ActivityViewRepository : IReadOnlyRepository<ActivityView, UUID> {
 
     fun findByActivityIdAndType(activityId: Long, type: EActivityType): ActivityView?
 }
