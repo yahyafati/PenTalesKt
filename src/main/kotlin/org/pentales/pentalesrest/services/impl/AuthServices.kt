e50b64bbce6f285c1a6ff17a59ddb79f3a7f3203
Yahya Fati <yfati037@gmail.com>
2024-06-03 15:48:04 +0300

    set background on account creation

--- src/main/kotlin/org/pentales/pentalesrest/services/impl/AuthServices.kt
+++ src/main/kotlin/org/pentales/pentalesrest/services/impl/AuthServices.kt
@@ -16,6 +16,21 @@ class AuthServices(
     private val authenticationFacade: IAuthenticationFacade
 ) : IAuthServices {
 
+    private val backgroundForegroundCombos = listOf(
+        "f44336" to "fff",
+        "e91e63" to "fff",
+        "2cb422" to "504848",
+        "2196f3" to "000000",
+        "ff9800" to "60a0a6",
+        "9c27b0" to "fff",
+        "3f51b5" to "fff",
+        "009688" to "fff",
+        "ff5722" to "fff",
+        "607d8b" to "fff",
+        "f44336" to "fff",
+        "e91e63" to "fff",
+    )
+
     override fun register(
         @Validated
         registerUser: RegisterUser
@@ -28,6 +43,15 @@ class AuthServices(
             throw ValidationException("Email already exists")
         }
         user.password = passwordEncoder.encode(user.password)
+
+        val profile = user.profile
+        if (profile != null) {
+            if (profile.profilePicture.isNullOrBlank()) {
+                val (background, foreground) = backgroundForegroundCombos.random()
+                profile.profilePicture =
+                    "https://ui-avatars.com/api/?name=${profile.firstName}+${profile.lastName}&background=$background&color=$foreground"
+            }
+        }
         return userServices.save(user)
     }
 
